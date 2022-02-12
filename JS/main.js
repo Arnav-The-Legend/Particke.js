1f24eef1b8cd228eed8d666dbf1e69776c508852
Arnav Singh <92202997+Arnav-The-Legend@users.noreply.github.com>
2022-02-12 19:16:56 +0530

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,9 @@
+console.log(`Index Screen Loaded`);
+console.log(window.location.href);
+console.log(`Screen Height: ${screen.height}`);
+console.log(`Screen Width: ${screen.width}`);
+console.log(`Window Height: ${window.outerHeight}`);
+console.log(`Window Width: ${window.outerWidth}`);
+console.log(`Content Height: ${window.innerHeight}`);
+console.log(`Content Width: ${window.innerWidth}`);
+
